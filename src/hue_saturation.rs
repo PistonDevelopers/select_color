162e1f50f3408ac9494e1ad1df59b62efa496cd4
Sven Nilsen <bvssvni@gmail.com>
2015-01-03 20:47:05 +0100

    Upgraded to latest Rust

--- src/hue_saturation.rs
+++ src/hue_saturation.rs
@@ -8,7 +8,7 @@ use image::{
 };
 
 /// Selects color at coordinate in a rectangle show hue and saturation
-pub fn select(rect: [u32, ..4], x: u32, y: u32) -> Rgba<u8> {
+pub fn select(rect: [u32; 4], x: u32, y: u32) -> Rgba<u8> {
     let inv_rect_width = 1.0_f64 / rect[2] as f64;
     let half_inv_rect_height = 0.5_f64 / rect[3] as f64;
     let inv60 = 1.0_f64 / 60.0_f64;
@@ -53,7 +53,7 @@ pub fn select(rect: [u32, ..4], x: u32, y: u32) -> Rgba<u8> {
 /// while saturated colors are at the bottom.
 pub fn fill_image(
     image: &mut RgbaImage,
-    rect: [u32, ..4]
+    rect: [u32; 4]
 ) {
     let inv_rect_width = 1.0_f64 / rect[2] as f64;
     let half_inv_rect_height = 0.5_f64 / rect[3] as f64;
